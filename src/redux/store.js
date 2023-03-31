1f3b45f3a09d4eb8764f74b11a7a98ad9b508a75
AleksPastukhov <passandro1990@gmail.com>
2023-03-31 23:04:05 +0300

    del comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import { persisterContactsReduser } from './contactsSlice';
-// import { filterSlice } from './filterSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persisterContactsReduser,
-//     filter: filterSlice.reducer,
-//   },
-//   middleware(getDefaultMiddleware) {
-//     return getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     });
-//   },
-// });
-
-// export const persistor = persistStore(store);
-
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from 'redux/filterSlice';
 import { contactsReducer } from 'redux/contactsSlice';
